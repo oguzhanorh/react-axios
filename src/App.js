b580f5c8a745bdca4db1c7151eba777458e10ed8
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-14 17:34:43 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Title from './components/Title';
+import Setup from './examples/4-global-instance';
+function App() {
+  return (
+    <main>
+      <Title />
+      <Setup />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/axios/interceptors.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+//karmaşık uygulamalarda sıklıkla kullanıyoruz örneğin kimlik doğrulama gibi.
+
+const authFetch = axios.create({
+  baseURL: 'https://course-api.com',
+});
+
+authFetch.interceptors.request.use(
+  (request) => {
+    request.headers.common['Accept'] = 'application/json';
+    console.log('request sent');
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+authFetch.interceptors.response.use(
+  (response) => {
+    console.log('got response');
+    return response;
+  },
+  (error) => {
+    console.log(error.response);
+    if (error.response.status === 404) {
+      // do something
+      console.log('NOT FOUND');
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default authFetch;
